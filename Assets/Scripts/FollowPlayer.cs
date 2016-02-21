04e2cc6e9a2a5264938b82260d696762c29403aa
Edgar G Rubio <ed.ga.ru90@gmail.com>
2016-02-21 13:07:32 +0100

    Fix camera follow

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,7 +8,12 @@ public class FollowPlayer : MonoBehaviour {
 	public float yOffset;
 	public float zOffset;
 
+	void Start (){
+		target = GameObject.FindGameObjectWithTag ("Player");
+	}
+
 	void LateUpdate() {
+		//Debug.Log (target.transform.position.x);
 		gameObject.transform.position = new Vector3(target.transform.position.x + xOffset,
 		                                      4,
 		                                      target.transform.position.z + zOffset);
