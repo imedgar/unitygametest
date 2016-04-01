e64e14b761527a9dbd356d77d901a2c1a9d0672e
Edgar G Rubio <ed.ga.ru90@gmail.com>
2016-04-01 21:36:07 +0200

    Chimeneas TODO* Añadir random spawn y posicion

---
+++ Assets/Scripts/AddObstacle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddObstacle : MonoBehaviour {
+	
+	[SerializeField]
+	GameObject prefabToAdd;
+	
+	// Use this for initialization
+	void Start () {
+		AddChimney ();	
+	}
+	
+	void AddChimney (){
+		float sizeY = gameObject.GetComponent<BoxCollider2D>().size.y;
+		float yPrefabPosition = transform.position.y + 
+			( gameObject.GetComponent<BoxCollider2D>().size.y / 2 ) +
+			( prefabToAdd.GetComponent<BoxCollider2D>().size.y / 2 );
+		(Instantiate (prefabToAdd, 
+			new Vector3(gameObject.transform.position.x, yPrefabPosition, gameObject.transform.position.z), 
+			Quaternion.Euler(0, 0, 0)) 
+			as GameObject).transform.parent = gameObject.transform;
+	}
+}
